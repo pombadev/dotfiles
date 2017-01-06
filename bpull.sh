660766c7cf23f1ee76d6726cc6b0ae3028eceff8
PombaM <pomba.magar@gmail.com>
2017-01-06 07:53:33 +0000

    Update bpull.sh

--- bpull.sh
+++ bpull.sh
@@ -21,7 +21,7 @@ STATUS_4=$?
 
 if [[ $STATUS_1 = 0 && $STATUS_2 = 0 && $STATUS_3 = 0 && $STATUS_4 = 0 ]] ; then
 
-	read -p 'Start dev/watch/continue? <d/w/c> ' PROMPT
+	read -p 'Start dev/watch/continue? <d/w/x> ' PROMPT
 
 	if [ "$PROMPT" = 'd' ]; then
 		grunt dev:dev &
@@ -31,8 +31,8 @@ if [[ $STATUS_1 = 0 && $STATUS_2 = 0 && $STATUS_3 = 0 && $STATUS_4 = 0 ]] ; then
 		grunt run:dev &
 	fi
 
-	if [[ "$PROMPT" = 'c' ]]; then
-		cowsay -f stegosaurus 'Everything is up-to-date'
+	if [[ "$PROMPT" = 'x' ]]; then
+		echo -e '\nEverything is up-to-date\n'
 	fi
 else
 	notify-send 'Something is not quite right, check the terminal for more info!'
