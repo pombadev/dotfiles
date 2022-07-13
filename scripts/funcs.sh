48f1ae5d75df5e67ec85483d533a1a3623e4ba7d
pjmp <11157420+pjmp@users.noreply.github.com>
2022-07-13 10:48:26 +0545

    backup: Wed Jul 13 10:48:26 AM +0545 2022

--- scripts/funcs.sh
+++ scripts/funcs.sh
@@ -369,3 +369,71 @@ less() {
 		command less "$OPTS" "$@"
 	fi
 }
+
+jrepl () {
+	if [[ -t 0 ]]; then
+		echo '' | fzf --disabled --print-query --preview "jq --color-output {q} $1" --preview-window='up:99%'
+	else
+		temp=$(mktemp)
+		cat "$@" | tee "$temp" &> /dev/null
+		echo '' | fzf --disabled --print-query --preview "jq --color-output {q} $temp" --preview-window='up:99%'
+		rm "$temp"
+	fi
+}
+
+generate-license () {
+	local cache_dir="$HOME/.cache/licenses_generator"
+
+	if [[ ! -d "$cache_dir" ]]; then
+		echo "Creating cache directory in: $cache_dir"
+		mkdir "$cache_dir"
+	else
+		echo "Cache directory exit: $cache_dir"
+	fi
+
+	if [[ ! -f "$cache_dir/licenses.json" ]]; then
+		echo "Fetching available licenses list"
+		curl -s https://api.github.com/licenses >"$cache_dir/licenses.json"
+	else
+		echo "Licenses list exist in cache directory: $cache_dir/licenses.json"
+	fi
+
+	licenses=$(jq --raw-output '.[].key' "$cache_dir/licenses.json" | fzf --tac --multi --reverse)
+
+	if [ -z "$licenses" ]; then
+		echo "License(s) not selected, cannot proceed forward"
+		exit 1
+	fi
+
+	printf "Selected license(s):\n%s" "${licenses^^}"
+
+	for license in $licenses; do
+		if [[ ! -f "$cache_dir/$license.json" ]]; then
+			echo "Fetching ${license^^}"
+			curl -s "https://api.github.com/licenses/$license" >"$cache_dir/$license.json"
+		else
+			echo "${license^^} exist in cache"
+		fi
+
+		license_contents=$(jq --raw-output '.body' "$cache_dir/$license.json")
+
+		if [ "$license_contents" == "null" ]; then
+			echo "License contents missing, please delete $cache_dir and try again"
+		else
+			echo "Writing 'LICENSE-${license^^}' to '$(pwd)'"
+			echo "$license_contents" >"LICENSE-${license^^}"
+
+		fi
+	done
+}
+
+generate-gitignore() {
+	local base_url="https://www.toptal.com/developers/gitignore/api"
+	local list_url="$base_url/list?format=lines"
+
+	local selections=$(curl -s "$list_url" | fzf --reverse --multi --preview "curl -s $base_url/{}")
+
+	for selection in $selections; do
+		curl -s "$base_url/$selection" >> .gitignore
+	done
+}
