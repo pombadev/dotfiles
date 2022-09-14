cf22531da853b3f2a02f8fcec7a4a4eb57708dcf
Pomba Magar <11157420+pombadev@users.noreply.github.com>
2022-09-14 15:33:51 +0545

    clone to a directory

--- mero/installer.sh
+++ mero/installer.sh
@@ -11,7 +11,7 @@ alias cmx='git --git-dir=$HOME/dotfiles --work-tree=$HOME'
     cd ~
 
     # note: `--recurse-submodules` doesn't work with `--bare` option
-    git clone --bare https://github.com/pombadev/dotfiles.git
+    git clone --bare https://github.com/pombadev/dotfiles.git dotfiles
 
     echo "WARNING! WILL OVERWRITE FILES ON LOCAL IF IT EXIST?"
     echo "Select 1/2 to continue"
