ee78145a85a9591a14460115716593b1a1e010f8
pombadev <11157420+pombadev@users.noreply.github.com>
2024-05-15 11:14:47 +0545

    backup on: Wed May 15 11:14 AM 2024

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,26 @@
 local wezterm = require 'wezterm'
 
 local config = {
+  default_prog = {'zsh', '-l'},
 	window_background_opacity = 0.9,
 	font_size = 10,
 	cursor_thickness = 0.1,
+  enable_scroll_bar = true,
 }
 
+-- custom keymap
+config.keys = {
+  {
+    key = 'h',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'v',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+
+}
 
 return config
