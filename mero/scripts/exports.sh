80613a23d60e98847f1ba7b0e6cef32e801b71c0
pombadev <11157420+pombadev@users.noreply.github.com>
2024-09-09 09:28:22 +0545

    backup on: 09 Mon Sep 2024 09:28 AM

--- mero/scripts/exports.sh
+++ mero/scripts/exports.sh
@@ -102,3 +102,11 @@ fi
 export PATH="$__PATH__:$PATH"
 
 export EDITOR=nvim
+
+# pnpm
+export PNPM_HOME="/home/pjmp/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
