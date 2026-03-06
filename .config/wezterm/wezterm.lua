f877e8f9813b5aa24a1c48b44d58629b1da3aef9
pombadev <11157420+pombadev@users.noreply.github.com>
2026-03-06 22:43:28 +0545

    backup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,24 @@
 local wezterm = require "wezterm"
 
-local config = {
-  default_prog = {"zsh", "-l"},
-  window_background_opacity = 0.9,
-  font_size = 10,
-  cursor_thickness = 0.1,
-  enable_scroll_bar = true
-}
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+-- config.initial_cols = 120
+-- config.initial_rows = 28
+
+-- -- or, changing the font size and color scheme.
+-- config.font_size = 10
+
+config.enable_wayland = false
+config.window_background_opacity = 0.7
+config.window_decorations = "NONE"
+
 
 -- custom keymap
 config.keys = {
