49aefac1e016cc975d96b880ce8b2ef58d893072
pombadev <11157420+pombadev@users.noreply.github.com>
2026-03-23 22:21:53 +0545

    Mon Mar 23 10:21:53 PM +0545 2026

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require "wezterm"
 
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local action = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -16,27 +15,87 @@ local config = wezterm.config_builder()
 -- config.font_size = 10
 
 config.enable_wayland = false
-config.window_background_opacity = 0.7
-config.window_decorations = "NONE"
+config.window_background_opacity = 0.9
+-- config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.enable_scroll_bar = true
+-- config.enable_wayland = true
+config.front_end = "WebGpu"
+config.enable_kitty_keyboard = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+-- config.enable_tab_bar = true
 
+config.inactive_pane_hsb = {
+    brightness = 0.2
+}
+
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+    local index = ''
+    if #tabs > 1 then
+        index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+    end
+
+    return index .. tab.active_pane.title
+end)
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local pane = tab.active_pane
+    local tab_index = tab.tab_index + 1
+
+    local process_name = pane.foreground_process_name or ""
+    process_name = process_name:gsub('[/\\]+$', '') -- strip trailing slashes
+    process_name = process_name:match('([^/\\]+)$') or process_name
+
+    local cwd = ""
+    local cwd_uri = pane.current_working_dir
+    if cwd_uri then
+        cwd = cwd_uri.file_path
+        cwd = cwd:gsub('[/\\]+$', '') -- strip trailing slashes (this fixes the full path bug!)
+        cwd = cwd:match('([^/\\]+)$') or cwd
+    end
+    if cwd == "" then
+        cwd = "~"
+    end
+
+    local title_text = string.format(' %d: %s @ %s ', tab_index, cwd, process_name)
+
+    return title_text
+end)
+
+config.colors = {
+    background = "#000000",
+    split = '#fff700'
+}
 
 -- custom keymap
-config.keys = {
-  {
+config.keys = {{
     key = "h",
     mods = "CTRL|ALT",
-    action = wezterm.action.SplitHorizontal {domain = "CurrentPaneDomain"}
-  },
-  {
+    action = action.SplitHorizontal {
+        domain = "CurrentPaneDomain"
+    }
+}, {
     key = "v",
     mods = "CTRL|ALT",
-    action = wezterm.action.SplitVertical {domain = "CurrentPaneDomain"}
-  },
-  {
-    key = "d",
-    mods = "CTRL|ALT",
-    action = wezterm.action.PopKeyTable
-  }
-}
+    action = action.SplitVertical {
+        domain = "CurrentPaneDomain"
+    }
+}, {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = action.ActivatePaneDirection 'Left'
+}, {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = action.ActivatePaneDirection 'Right'
+}, {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = action.ActivatePaneDirection 'Up'
+}, {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = action.ActivatePaneDirection 'Down'
+}}
 
 return config
